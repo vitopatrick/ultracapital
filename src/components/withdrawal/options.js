f2ab810962a16e81b06021212ef6a899036416a2
theWarlock <fecypher@gmail.com>
2023-04-04 23:09:09 +0100

    changed amount

--- src/components/withdrawal/options.js
+++ src/components/withdrawal/options.js
@@ -1,22 +1,22 @@
 export const options = [
   {
     name: "USDT",
-    minAmount: "$1000",
+    minAmount: "$7000",
     duration: "48hrs",
   },
   {
     name: "Bitcoin",
-    minAmount: "$1000",
+    minAmount: "$7000",
     duration: "48hrs",
   },
   {
     name: "Litecoin",
-    minAmount: "$1000",
+    minAmount: "$7000",
     duration: "48hrs",
   },
   {
     name: "Shiba",
-    minAmount: "$1000",
+    minAmount: "$7000",
     duration: "48hrs",
   },
 ];
