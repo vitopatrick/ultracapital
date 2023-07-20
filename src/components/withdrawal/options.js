b86d4077140090568aea4c55052e25a69565603a
theWarlock <patrickvito2012@gmail.com>
2023-07-20 02:58:10 +0100

    second commit

--- src/components/withdrawal/options.js
+++ src/components/withdrawal/options.js
@@ -1,22 +1,22 @@
 export const options = [
   {
     name: "USDT",
-    minAmount: "$7000",
+    minAmount: "$1000",
     duration: "48hrs",
   },
   {
     name: "Bitcoin",
-    minAmount: "$7000",
+    minAmount: "$1000",
     duration: "48hrs",
   },
   {
     name: "Litecoin",
-    minAmount: "$7000",
+    minAmount: "$1000",
     duration: "48hrs",
   },
   {
     name: "Shiba",
-    minAmount: "$7000",
+    minAmount: "$1000",
     duration: "48hrs",
   },
 ];
